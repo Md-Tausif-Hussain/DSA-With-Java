0cb0cab7f0d9bf2276741d3ea851bcd829767ffb
Md Tausif Hussain <mdtausifhussain.dev@gmail.com>
2025-06-14 13:39:38 +0530

    Update String_Palindrome.java

--- String/String_Palindrome.java
+++ String/String_Palindrome.java
@@ -1,6 +1,9 @@
 import java.util.*;
+
 public class String_Palindrome {
+    
     public static boolean isPalindrome(String str){
+        
         for(int i =0 ; i<str.length()/2;i++){
             //int n = str.length();
             if (str.charAt(i) != str.charAt(str.length()-1-i)){
@@ -10,6 +13,7 @@ public class String_Palindrome {
         }
         return true;
     }
+    
     public static void main(String[] args) {
         String str = "racecar";
         System.out.println(isPalindrome(str));
